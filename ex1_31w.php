233c4dcaa06396e523f41619fd2882da58f1ddff
Bonenfant <e2095741@cmaisonneuve.qc.ca>
2022-06-02 11:40:31 -0400

    s3-c2 Configuration du github et premier commit

---
+++ ex1_31w.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * Plugin Name: Plugin de l'exercice #1
+ * Author: Jonathan Bonenfant
+ * Description: Premier plugin pour l'exercice #1
+ * Author URI: https://github.com/Jambonneaux/31w-exer1
+ */
+
+?>
+
+<?php 
+
+
+
+?>
